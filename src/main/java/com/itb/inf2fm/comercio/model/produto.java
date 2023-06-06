9fe20d326081f6aa7cb1bd5ded10fec6d32c4ab3
gioohOliveira <go4267994@gmail.com>
2023-06-06 12:29:40 -0300

    Inicio controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/model/produto.java
@@ -0,0 +1,19 @@
+package com.itb.inf2fm.comercio.model;
+
+public class produto {
+
+	
+	private long id;
+	private String nome;
+	private String descricao;
+	private String codigobarras;
+	private double preço;
+	
+	// public: Acesso liberado para todas as classes
+	
+	// private: Acesso permitido apenas pelos membros  
+	// da própria classe.
+	// Entende-se por membros(atributos e ou métodos)
+	
+	
+}
